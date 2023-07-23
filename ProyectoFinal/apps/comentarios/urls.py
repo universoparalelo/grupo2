e39769bc14528e70a4823b52710575e67fe8c82f
UniversoParalelo <cmartinrodich@gmail.com>
2023-07-23 11:51:29 -0300

    MB con comentarios y pequeños arreglos en contacto

--- ProyectoFinal/apps/comentarios/urls.py
+++ ProyectoFinal/apps/comentarios/urls.py
@@ -6,4 +6,11 @@ app_name = 'comentarios'
 urlpatterns = [
     # Agregar comentario
     path('agregar-com/<int:pk>', views.Agregar, name='agregar_comentarios'),
+
+    # Modificar comentario
+    path('borrar-comentario/<int:pk>', views.BorrarComentario.as_view(), name="borrar_comentario"),
+
+    # Borrar comentario
+    path('modificar-comentario/<int:pk>', views.ModificarComentario.as_view(), name="modificar_comentario"),
+
 ]
--- ProyectoFinal/apps/comentarios/views.py
+++ ProyectoFinal/apps/comentarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from apps.noticias.models import Noticia
 from apps.comentarios.models import Comentario
+from django.views.generic import DeleteView, UpdateView
+from .forms import Form_Modificacion
 
 from django.urls import reverse_lazy
 
@@ -14,3 +16,14 @@ def Agregar(request,pk):
 	Comentario.objects.create(texto = com, usuario = usuario, noticia = noticia)
 
 	return HttpResponseRedirect(reverse_lazy('noticias:detalle_noticia' , kwargs={'pk':pk}))
+
+class BorrarComentario(DeleteView):
+	model = Comentario
+	success_url = reverse_lazy('noticias:misNoticias')
+	
+
+class ModificarComentario(UpdateView):
+	model = Comentario
+	form_class = Form_Modificacion
+	template_name = 'noticias/modificar.html'
+	success_url = reverse_lazy('noticias:misNoticias')
